b85467eb5d061fee8e6275ed756d18b963716a66
Sally Haefling <sallyhaefling@gmail.com>
2019-05-28 18:50:09 -0600

    Attempt to fetch data and returns oauth error

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,6 +1,6 @@
 import React, { Component }from 'react';
 import './_App.scss';
-import { apikey } from '../../utils/apikey';
+import { apiKey } from '../../utils/apikey.js';
 import Header from '../../components/Header/index.js';
 import { connect } from 'react-redux';
 
@@ -11,6 +11,14 @@ export default class App extends Component {
 
     }
   }
+
+  componentDidMount = () => {
+    const url = `https://api.petfinder.com/v2/animals/api_key=${apiKey}&language=en-US&page=1`
+    //having issues with a CORS error - this mode below doesnt seem to help anything. Tried using mode: 'cors' and still wont work. Added chrome extension to try and bypass issues, but, still wont resolve and response is giving me back none of the data I am looking for.
+    fetch(url, {mode: 'no-cors'})
+    .then(response => console.log('response', response))
+  }
+
   render() {
     return (
       <section className='App'>
