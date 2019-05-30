bb70569eca55c7458463950e89991422b5e56b34
Sally Haefling <sallyhaefling@gmail.com>
2019-05-30 08:53:39 -0600

    Start redux setup

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import './index.scss';
 import App from './containers/App/index.js';
 import { BrowserRouter } from 'react-router-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { rootReducer } from './reducers';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(rootReducer, composeWithDevTools())
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, 
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>, 
 document.getElementById('root'));
 
 serviceWorker.unregister();
---
+++ src/reducers/animals-reducer.js
@@ -0,0 +1,10 @@
+const initialState = []
+
+const animalsReducer = (state=initialState, action) => {
+  // switch(action.type) {
+  //   case '' :
+  //     return 
+  // }
+}
+
+export default animalsReducer;
