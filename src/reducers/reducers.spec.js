95c1fa942eb0c715a238880bc1807fbbd0dc04a6
Sally Haefling <sallyhaefling@gmail.com>
2019-06-01 13:23:09 -0600

    Test animalsReducer

--- src/reducers/reducers.spec.js
+++ src/reducers/reducers.spec.js
@@ -1,10 +1,19 @@
 import { animalsReducer } from './animals-reducer';
 import * as actions from '../actions';
+import { mockAnimals } from '../utils/mockData';
 
 describe('animalsReducer', () => {
   it('should return initialState', () => {
     const expected = []
     const result = animalsReducer(undefined, {})
+
+    expect(result).toEqual(expected)
+  });
+
+  it('should return the state with set animals', () => {
+    const expected = mockAnimals
+
+    const result = animalsReducer(undefined, actions.setAnimals(mockAnimals))
 
     expect(result).toEqual(expected)
   })
